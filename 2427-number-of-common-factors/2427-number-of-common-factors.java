ad74e0ed55e72eb632e9dbd18498ab593db48272
Jigyashu Suraj <56422458+jigyashu-suraj@users.noreply.github.com>
2022-10-14 19:18:33 +0530

    Time: 0 ms (100.00%), Space: 41 MB (37.27%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.java
+++ 2427-number-of-common-factors/2427-number-of-common-factors.java
@@ -1,17 +1,16 @@
 class Solution {
     public int commonFactors(int a, int b) {
-        int n,count=0;
-        if(a>=b)
-            n = a;
-        else 
-            n = b;
-        for(int i=1;i<=n/2;i++)
+        int count=1;
+        for(int i=2;i<=gcd(a,b);i++)
             if(a%i ==0 && b%i == 0){
                 count++;
-                System.out.print(i+" ");
             }
-        if(a == b)
-            count++;
         return count;
+    }
+    int gcd(int a, int b){
+        if(a==0){
+            return b;
+        }
+        return gcd(b%a, a);
     }
 }
