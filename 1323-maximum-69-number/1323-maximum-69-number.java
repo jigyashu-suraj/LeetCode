140520f55dec2474ce6dadca9617170feb86df29
Jigyashu Suraj <56422458+jigyashu-suraj@users.noreply.github.com>
2022-10-16 11:41:57 +0530

    Time: 1 ms (89.60%), Space: 40.8 MB (69.63%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.java
+++ 1323-maximum-69-number/1323-maximum-69-number.java
@@ -1,21 +1,12 @@
 class Solution {
     public int maximum69Number (int num) {
-        int max = num;
-        StringBuilder sb = new StringBuilder(Integer.toString(max));
-		StringBuilder s = new StringBuilder();
+        StringBuilder sb = new StringBuilder(Integer.toString(num));
 		for (int i = 0; i < sb.length(); i++) {
-			s.setLength(0);
-			s.append(sb);
-			if (s.charAt(i) == '9')
-				s.setCharAt(i, '6');
-			else
-				s.setCharAt(i, '9');
-			int temp = Integer.parseInt(s.toString());
-			if (temp > max) {
-				max = temp;
-			}
-
+			if (sb.charAt(i) == '6'){
+				sb.setCharAt(i, '9');
+                break;
+            }
 		}
-        return max;
+        return Integer.parseInt(sb.toString());
     }
 }
