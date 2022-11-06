62acbf93ffc754188d549e44598f2dc03d109044
Jigyashu Suraj <56422458+jigyashu-suraj@users.noreply.github.com>
2022-11-06 23:07:51 +0530

    Time: 163 ms (8.51%), Space: 45.8 MB (5.46%) - LeetHub

--- 1952-three-divisors/1952-three-divisors.java
+++ 1952-three-divisors/1952-three-divisors.java
@@ -1,12 +1,10 @@
 class Solution {
     public boolean isThree(int n) {
         int count = 0;
-        for(int i =2;i<=n/2;i++){
+        for(int i =2;i<=n/2 && count<2;i++){
             System.out.println(i);
             if(n%i==0)
                 count++;
-            else if(count>=2)
-                break;
         }
         if(count==1)
             return true;
