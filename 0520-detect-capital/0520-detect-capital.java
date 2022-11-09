e6b9345c4cd2a30565be4718e7f8a09a34f4d6ed
Jigyashu Suraj <56422458+jigyashu-suraj@users.noreply.github.com>
2022-11-09 21:05:06 +0530

    Time: 2 ms (83.10%), Space: 42.4 MB (51.39%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        int count =0, n = word.length(),count2 = 0,count3 =0;
+        for(int i = 0;i<n;i++){
+            if(word.charAt(i)>=65 && word.charAt(i)<= 90)
+                count++; 
+            else if(word.charAt(i)>=97 && word.charAt(i)<= 122)
+                count2++;
+        }
+        if(count == n ||count2 == n)
+        return true;
+        else if(count == 1 && (word.charAt(0)>=65 && word.charAt(0)<= 90))
+            return true;
+        else 
+            return false;
+    }
+}
