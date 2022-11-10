7dcd999fad6c7dcd2724d03be08bb4a25d6d72ba
Jigyashu Suraj <56422458+jigyashu-suraj@users.noreply.github.com>
2022-11-10 17:51:48 +0530

    Time: 5 ms (70.60%), Space: 43.7 MB (65.69%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,22 +1,25 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-         ArrayList<Integer> a = new ArrayList<Integer>();
         Arrays.sort(nums1);
         Arrays.sort(nums2);
-        int i = 0 , j = 0;
-        while(i < nums1.length && j < nums2.length){
-            if(nums1[i] > nums2[j])
-                j++;
-            else if(nums2[j] > nums1[i])
+        int n = nums1.length, m = nums2.length;
+        int i = 0, j = 0;
+        List<Integer> list = new ArrayList<>();
+        while(i < n && j < m){
+            int a = nums1[i], b= nums2[j];
+            if(a == b){
+                list.add(a);
                 i++;
-            else{
-                a.add(nums1[i]);
-                i++;j++;
+                j++;
+            }else if(a < b){
+                i++;
+            }else{
+                j++;
             }
         }
-        int[] res = new int[a.size()];
+        int[] res = new int[list.size()];
         i = 0;
-        for(int r:a)
+        for(int r:list)
             res[i++]=r;
         return res;
         
