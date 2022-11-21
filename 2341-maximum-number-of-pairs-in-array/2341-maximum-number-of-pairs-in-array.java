b7d861e7cdcc1a2d2fa4f0360be80a3e280145f4
Jigyashu Suraj <56422458+jigyashu-suraj@users.noreply.github.com>
2022-11-21 20:11:30 +0530

    Time: 5 ms (14.14%), Space: 42.9 MB (34.92%) - LeetHub

--- 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.java
+++ 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.java
@@ -1,19 +1,17 @@
 class Solution {
     public int[] numberOfPairs(int[] nums) {
     List<Integer> a = new ArrayList<Integer>();
-    int n = nums.length,count=0, l[] = new int[2] ;
-    for(int i=0;i<n;i++)
-        a.add(nums[i]);
-    for(int i = 0;i<a.size();){
+    int n = nums.length,count=0,i=0,l[] = new int[2] ;
+    for(int j=0;j<n;j++)
+        a.add(nums[j]);
+    Collections.sort(a);
+    while(i<a.size()-1){
        boolean flag = true;
-        for(int j = i+1;j<a.size();j++){
-            if(a.get(i) == a.get(j)){
-                count++;
-                a.remove(i);
-                a.remove(j-1);
-                flag = false;
-                break;
-            }
+       if(a.get(i) == a.get(i+1)){
+            count++;
+            a.remove(i);
+            a.remove(i);
+            flag = false;
         }
         if(flag==true){
             i++;
