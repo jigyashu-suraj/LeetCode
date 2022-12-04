de9bf02612f7aea22f805fa17402876526c0ee40
Jigyashu Suraj <56422458+jigyashu-suraj@users.noreply.github.com>
2022-12-04 23:02:16 +0530

    Time: 4 ms (60.00%), Space: 42.2 MB (100.00%) - LeetHub

--- 2490-circular-sentence/2490-circular-sentence.java
+++ 2490-circular-sentence/2490-circular-sentence.java
@@ -8,6 +8,26 @@ class Solution {
             }
         }
         return sentence.charAt(0)==sentence.charAt(n);
+        
+        //  String[] word = sentence.split(" ");
+        // int n = word.length-1;  
+        // boolean flag = false;
+        // if(sentence.charAt(0)==sentence.charAt(sentence.length()-1)){
+        //     flag = true;
+        // }
+        // if(n>0){
+        //     for(int i = 0;i<n;i++){
+        //         char l = word[i].charAt(word[i].length()-1);
+        //         char k = word[i+1].charAt(0);
+        //         if(l != k){
+        //             flag = false;
+        //              break;
+        //     }
+        // }
+        // }
+        // if(flag)
+        //     return true;
+        // return false;
     }
 }
     
