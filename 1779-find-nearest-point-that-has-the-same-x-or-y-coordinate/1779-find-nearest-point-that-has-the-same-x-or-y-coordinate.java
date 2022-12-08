74035c1efe42590fb78c507256580d9633ee1d3d
Jigyashu Suraj <56422458+jigyashu-suraj@users.noreply.github.com>
2022-12-08 21:36:18 +0530

    Time: 6 ms (51.83%), Space: 74 MB (28.71%) - LeetHub

--- 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.java
+++ 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.java
@@ -3,8 +3,8 @@ class Solution {
         int min = Integer.MAX_VALUE, index = -1;
         for(int i = 0;i<points.length;i++){
             int x2 = points[i][0], y2=points[i][1];
-            if(x== x2|| y == y2){
-                int distance  = Math.abs((x-x2)+(y-y2));
+            if(x== x2 || y == y2){
+                int distance  = Math.abs(x-x2+y-y2);
                 if(distance<min){
                     min = distance;
                     index = i;
